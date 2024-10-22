26e2d1eda684394eaa931681727252b61ef08dfa
Alerrandoribeiro <166244101+Alerrandoribeiro@users.noreply.github.com>
2024-10-22 18:00:14 -0300

    Delete exercicios-frontend-ii-senai directory

--- exercicios-frontend-ii-senai/src/paginas/Multiplicar/Multiplicar.jsx
+++
@@ -1,8 +0,0 @@
-import { useState } from "react"
-
-const Multiplicar = () => {
-    const [numeroUm,setNumeroUm] = useState('');
-    const [numeroDois,setNumeroDois] = useState('');
-    const [resultado,setResultado] = useState('');
-
-}
